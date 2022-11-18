ea233957d9af9b81de6ef1400c42068b41e446f5
Adriano Volter <91354299+AdrianoVolter@users.noreply.github.com>
2022-11-18 15:02:38 +0000

    Comitando de container do vs code

---
+++ aula-1/script.js
@@ -0,0 +1,14 @@
+class Pessoa{
+    constructor(p_nome){
+        this.nome =p_nome
+        
+    }
+}
+
+let p1 = new Pessoa("Adriano")
+let p2 = new Pessoa("Jose ")
+let p3 = new Pessoa("Volter")
+
+console.log(p1.nome)
+console.log(p2.nome)
+console.log(p3.nome)
