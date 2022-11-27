e878587a1de412a8e481262792b0d3ab258f4f49
Adriano Volter <91354299+AdrianoVolter@users.noreply.github.com>
2022-11-27 20:10:57 +0000

    Usando funções no lugar de classes

---
+++ Aula60/script.js
@@ -0,0 +1,57 @@
+//Usar funçoes no lugar de classes 
+function Pessoa(p_nome, p_idade){
+    this.nome = p_nome,
+    this.idade = p_idade,
+    
+    getNome = function(){
+        return this.nome
+    }
+    getIdade = function(){
+        return this.idade
+    }
+    
+    //Setar a propriedade
+    setNome = function(nome){
+        this.nome = nome
+    }
+    setIdade = function(idade){
+        this.idade = idade
+    }
+
+    info(){//Imprime todas as informações do Carro
+        console.log("\n")
+        console.log(`Nome: ${this.nome}`)
+        console.log(`Idade: ${this.idade}`)
+        console.log("\n")
+    }
+}
+
+let pessoas = []
+
+const btn_add =  document.querySelector("#btn_add")
+const res =  document.querySelector(".res")
+
+const addPessoas = () => { 
+    res.innerHTML =""
+    pessoas.map((p)=>{
+        const div = document.createElement("div")
+        div.setAttribute("class","pessoa")
+        div.innerHTML = `Nome: ${p.getNome()}<br/>Idade: ${p.getIdade()}<br/>`
+        res.appendChild(div)
+        
+    })
+}
+
+
+btn_add.addEventListener("click",(evt)=>{
+    const nome = document.querySelector("#f_nome")
+    const idade =  document.querySelector("#f_idade")
+    const p = new Pessoa(nome.value , idade.value)
+    pessoas.push(p)
+    nome.value = ""
+    idade.value = ""
+    nome.focus()
+    addPessoas()
+    console.log(pessoas)
+
+})
