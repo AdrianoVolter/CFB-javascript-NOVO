e651b44f905c65061975c495ded2f358518a9559
Adriano Volter <91354299+AdrianoVolter@users.noreply.github.com>
2022-12-09 01:33:22 +0000

    Aula 63 criando objetos ,exercicio carros

--- Aula63/script.js
+++ Aula63/script.js
@@ -4,13 +4,13 @@ const f_blindagem = document.querySelector("#f_blindagem")
 const f_municao = document.querySelector("#f_municao")
 const carros = document.querySelector("#carros")
 const btn_addCarro = document.querySelector("#btn_addCarro")
-const f_nome =  document.querySelector("#f_nome")
+const f_nome = document.querySelector("#f_nome")
 const f_portas = document.querySelector("#f_portas")
 
 let a_carros = []
 
 //Quando escolher o carro militar
-f_tipoMilitar.addEventListener("click",(evt)=>{
+f_tipoMilitar.addEventListener("click", (evt) => {
     // f_nome = ""
     // f_portas = 0 
     f_blindagem.removeAttribute("disabled")
@@ -18,39 +18,36 @@ f_tipoMilitar.addEventListener("click",(evt)=>{
     console.log("Radio funcionando!")
 })
 //Quando escolher carro normal
-f_tipoNormal.addEventListener("click",(evt)=>{
+f_tipoNormal.addEventListener("click", (evt) => {
     f_nome = ""
     f_portas = 0
     f_blindagem.value = 0
-    f_municao.value  = 0
+    f_municao.value = 0
     f_blindagem.setAttribute("disabled", "disabled")
     f_municao.setAttribute("disabled", "disabled")
     console.log("Radio funcionando!")
 })
 //Gerencia os carros para colocar no html
-const gerenciarCarros=()=>{
+const gerenciarCarros = () => {
     carros.innerHTML = ""
-    a_carros.forEach((c)=>{
-        if(f_tipoNormal.checked){
-            const div = document.createElement("div")
-            div.setAttribute("class", "carro")
-            div.innerHTML = `Nome: ${c.nome}<br>Portas: ${c.portas} ` 
-            carros.appendChild(div)
-        }else{
-            const div = document.createElement("div")
-            div.setAttribute("class", "carro")
-            div.innerHTML = `Nome: ${c.nome}<br>Portas: ${c.portas}<br>Blindagem: ${c.blindagem}<br>Munição: ${c.municao} ` 
-            carros.appendChild(div)
-        }
-       
-    })   
+    a_carros.forEach((c) => {
+        const div = document.createElement("div")
+        div.setAttribute("class", "carro")
+        div.innerHTML = `Nome: ${c.nome}<br> `
+        div.innerHTML += `Portas: ${c.portas}<br>`
+        div.innerHTML += `Bindagem: ${c.blindagem}<br>`
+        div.innerHTML += `Minição: ${c.municao}<br>`
+        div.innerHTML += `Cor: ${c.cor}<br>`
+        
+        carros.appendChild(div)
+    })
 }
 
-btn_addCarro.addEventListener("click",(evt)=>{
-    if(f_tipoNormal.checked){
+btn_addCarro.addEventListener("click", (evt) => {
+    if (f_tipoNormal.checked) {
         const c = new Carro(f_nome.value, f_portas.value)
         a_carros.push(c)
-    }else{
+    } else {
         const c = new Militar(f_nome.value, f_portas.value, f_blindagem.value, f_municao.value)
         a_carros.push(c)
     }
@@ -60,7 +57,7 @@ btn_addCarro.addEventListener("click",(evt)=>{
 
 //Classe cria os carros 
 class Carro {  // Classe PAI
-    constructor(nome, portas){
+    constructor(nome, portas) {
         this.nome = nome
         this.portas = portas
         this.ligado = false
@@ -68,58 +65,58 @@ class Carro {  // Classe PAI
         this.cor = undefined
     }
 
-    ligar = function(){
+    ligar = function () {
         this.ligado = true
     }
 
-    desligar = function(){
-        this.ligado = false   
+    desligar = function () {
+        this.ligado = false
     }
-    
-    setCor = function(cor){
+
+    setCor = function (cor) {
         this.cor = cor
     }
 }
 
 class Militar extends Carro { //Classe filho 
-    constructor(nome, portas, blindagem, municao){
+    constructor(nome, portas, blindagem, municao) {
         super(nome, portas)
         this.blindagem = blindagem
         this.municao = municao
         this.setCor("Verde")
-   }
-   atirar = function(){
-    if(this.municao > 0){
-        this.municao--
     }
-   }
+    atirar = function () {
+        if (this.municao > 0) {
+            this.municao--
+        }
+    }
 }
 
-const c1  = new Carro('Normal', 4)
+// const c1 = new Carro('Normal', 4)
 
-const c2 = new Militar("Lutador",2,100,50 )
-//c2.ligar()
+// const c2 = new Militar("Lutador", 2, 100, 50)
+// //c2.ligar()
 //c2.nome = "Tanque"
 // c1.ligar()
 // c1.setCor("Preto")
 
-c2.atirar()
-c2.atirar()
-c2.atirar()
-c2.atirar()
-c2.atirar()
-c2.atirar()
+c.atirar()
+c.atirar()
+c.atirar()
+c.atirar()
+c.atirar()
+c.atirar()
 
-console.log(`\nNome do carro: ${c1.nome}`)
-console.log(`Quant. de portas: ${c1.portas}`)
-console.log(`Ligado: ${(c1.ligado? "Sim": "Não")}`)
-console.log(`Cor do carro: ${c1.cor}\n\n`)
+console.log(`\nNome do carro: ${c.nome}`)
+console.log(`Quant. de portas: ${c.portas}`)
+console.log(`Ligado: ${(c.ligado ? "Sim" : "Não")}`)
+console.log(`Cor do carro: ${c.cor}\n\n`)
 
 
 
-console.log(`\nNome do carro: ${c2.nome}`)
-console.log(`Quant. de portas: ${c2.portas}`)
-console.log(`Blindagem: ${c2.blindagem}`)
-console.log(`Munição: ${c2.municao}`)
-console.log(`Ligado: ${(c2.ligado? "Sim": "Não")}`)
-console.log(`Cor do carro: ${c2.cor}\n\n`)
+console.log(`\nNome do carro: ${c.nome}`)
+console.log(`Quant. de portas: ${c.portas}`)
+console.log(`Blindagem: ${c.blindagem}`)
+console.log(`Munição: ${c.municao}`)
+console.log(`Ligado: ${(c.ligado ? "Sim" : "Não")}`)
+console.log(`Cor do carro: ${c.cor}\n\n`)
