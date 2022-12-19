b051526173fea05c72d4b8050aa09c57931d9ea0
Adriano Volter <91354299+AdrianoVolter@users.noreply.github.com>
2022-12-19 21:09:43 +0000

    Statics , usando para definir um alerta

--- Aula66/script.js
+++ Aula66/script.js
@@ -1,6 +1,17 @@
 class Npc{
+    static alerta = false
     constructor(energia){
         this.energia = energia
+        //this.alerta = false
+    }
+
+    info = function(){
+        console.log(`Energia: ${this.energia}`)
+        console.log(`Alerta: ${Npc.alerta? "Sim ": "Não"}`)
+        console.log(`---------------`)
+    }
+    static alertar = function(){
+        Npc.alerta = true
     }
 }
 
@@ -9,6 +20,8 @@ const npc1 = new Npc(100)
 const npc2 = new Npc(80)
 const npc3 = new Npc(40)
 
-console.log(npc1.energia)
-console.log(npc2.energia)
-console.log(npc3.energia)
+//Npc.alerta = true
+
+npc1.info()
+npc2.info()
+npc3.info()
