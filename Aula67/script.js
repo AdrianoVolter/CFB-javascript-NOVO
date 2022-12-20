c9525801839b36c41d412ad711d5402f705e6ff8
Adriano Volter <91354299+AdrianoVolter@users.noreply.github.com>
2022-12-20 01:26:54 +0000

    Prototype , aprendendo sobre

--- Aula67/script.js
+++ Aula67/script.js
@@ -3,10 +3,21 @@ const Nave = function(energia){
     this.disparos = 100
 }
 
-Nave.prototype.vidas = 3
+Nave.prototype.vidas = 13
+Nave.prototype.disparar = function(){
+    if(this.disparos > 0){
+        this.disparos--
+    }
+}
 
-const n1 = new Nave(100)
+const n1 = new Nave(10)
 
-console.log(Nave)
-console.log(n1.energia)
+n1.disparar()
+n1.disparar()
+n1.disparar()
+n1.disparar()
+n1.disparar()
+
 console.log(n1)
+console.log(n1.energia)
+console.log(n1.disparos)
