f09b499c4cc57efe9e4ae571a4ddce3cc0295a88
Adriano Volter <91354299+AdrianoVolter@users.noreply.github.com>
2022-12-28 12:31:56 +0000

    Criando carro

--- Aula68/script.js
+++ Aula68/script.js
@@ -1,10 +1,30 @@
 //Polimorfismo
 class Carro{
-
+    constructor(tipo, estagioTurbo){
+        this.Turbo = new Turbo(1)
+        if(tipo == 1){
+            this.velMax = 120
+        }else if(tipo == 2){
+            this.velMax = 160
+        }else if(tipo == 3){
+            this.velMax = 200
+        }
+        this.velMax = this.Turbo
+    }
 }
 
 
 class Turbo{
-
+    constructor(e){
+        if(e == 0){
+            this.pot = 0
+        }else if(e == 1){
+            this.pot = 50
+        }else if(e == 2){
+            this.pot = 75
+        }else if(e == 3){
+            this.pot = 100
+        }
+    }
     
 }
